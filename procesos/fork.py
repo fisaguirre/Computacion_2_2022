fa80e7efeed86c3c7f57b53d9c57ddcc5e77fb5c
Fernando <fernando.isaguirre.13@gmail.com>
2022-04-04 19:06:01 -0300

    modo verboso

--- procesos/fork.py
+++ procesos/fork.py
@@ -9,35 +9,31 @@ parser.add_argument('--n', required=True, type=int, dest="cantidad_procesos", de
 	                    help='Escriba el numero de procesos hijos')
 parser.add_argument('--h', dest="ayuda",
 	                    help='Ayuda')
-parser.add_argument('--v', dest = "verboso", 
+parser.add_argument('--v', dest = "verboso", action="store_true",
 	                    help='Modo verboso')
 
 args = parser.parse_args()
 
-def suma(pid):
-    print ("esta es la función suma de pares")
+
+def suma(pid, ppid):
+    #print ("Esta es la función suma de pares")
     suma = 0
     for number in range(1, pid+1):
         if(number % 2 == 0):
             suma = suma + number
-    print ("la suma es:", suma)
+    print ("Suma:", str(pid) + " - " + str(ppid) + " = " + str(suma))
+    
 
 for numero in range(args.cantidad_procesos):
     pid = os.fork()
     if pid == 0:
-        print ("el pid es:", pid, os.getpid(), os.getppid())
-        print("se termina el hijo:" ,pid, os.getpid())
-        suma(os.getpid())
-        os._exit(0)
-
-"""
-listaProcesos = []
-for numero in range(args.cantidad_procesos):
-    pid = os.fork()
-    print ("primero:", pid, os.getpid(), os.getppid())
-    if pid == 0:
-        listaProcesos.append(pid)
-        print ("el pid es:", pid, os.getpid(), os.getppid())
+        if args.verboso:
+            print("Starting process: ", os.getpid())
+        suma(os.getpid(), os.getppid())
+        if args.verboso:
+            print("Ending process:", os.getpid())
+        exit()
+        #os._exit(0)
     else:
-        #os._exit(1)
-"""
+        os.wait()
+        
