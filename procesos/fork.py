8506cfa242ebdf9e51ea577856fdef6cd7b34884
Fernando <fernando.isaguirre.13@gmail.com>
2022-04-04 21:00:19 -0300

    Arreglado argparse

--- procesos/fork.py
+++ procesos/fork.py
@@ -5,35 +5,30 @@ import argparse
 import os.path
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--n', required=True, type=int, dest="cantidad_procesos", default = 2,
-	                    help='Escriba el numero de procesos hijos')
-parser.add_argument('--h', dest="ayuda",
-	                    help='Ayuda')
-parser.add_argument('--v', dest = "verboso", action="store_true",
-	                    help='Modo verboso')
+parser.add_argument('-n', "--numero", type=int, dest="cantidad_procesos", default = 2,
+	                    help='Escriba el numero de procesos hijos que desea')
+parser.add_argument('-v', "--verbose", dest = "verbose", action="store_true",
+	                    help='Habilitar modo verboso para mostrar el ciclo de vida de los procesos hijos')
 
 args = parser.parse_args()
 
 
 def suma(pid, ppid):
-    #print ("Esta es la función suma de pares")
     suma = 0
     for number in range(1, pid+1):
         if(number % 2 == 0):
             suma = suma + number
     print ("Suma:", str(pid) + " - " + str(ppid) + " = " + str(suma))
-    
 
 for numero in range(args.cantidad_procesos):
     pid = os.fork()
     if pid == 0:
-        if args.verboso:
+        if args.verbose:
             print("Starting process: ", os.getpid())
         suma(os.getpid(), os.getppid())
-        if args.verboso:
+        if args.verbose:
             print("Ending process:", os.getpid())
         exit()
         #os._exit(0)
     else:
         os.wait()
-        
